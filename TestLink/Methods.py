1f3dac317bc79fb562e1d0db771b274fd52d5be7
lex.khuat <lex.khuat@starixsoft.com>
2018-10-30 09:56:44 +0700

    Fix issue call log step without listener

--- TestLink/Methods.py
+++ TestLink/Methods.py
@@ -8,23 +8,28 @@ class Methods(object):
         Set logConsole to True for logged to console during run testcase\n
         Set stepPrefix to True to auto add prefix "Step:" and "Expectation:" before step content
         '''
-        iTC = BuiltIn().get_variable_value('${TESTLINK_iTC}')
-        if expectation is None: expectation = ''
-        if stepPrefix:
-            step = 'Step: %s' % step
-            expectation = 'Expectation: %s' % expectation
-        if logConsole:
-            txtMsg = step
-            if expectation != '': txtMsg = 'Step: %s\nVP: %s' % (step, expectation) 
-            BuiltIn().log_to_console(txtMsg)
-        iTC.setSteps(step, expectation)
+        try:            
+            iTC = BuiltIn().get_variable_value('${TESTLINK_iTC}')
+            if expectation is None: expectation = ''
+            if stepPrefix:
+                step = 'Step: %s' % step
+                expectation = 'Expectation: %s' % expectation
+            if logConsole:
+                txtMsg = step
+                if expectation != '': txtMsg = 'Step: %s\nVP: %s' % (step, expectation) 
+                BuiltIn().log_to_console(txtMsg)
+            iTC.setSteps(step, expectation)
+        except Exception, err:
+            pass
 
     def register_testlink_id(self, *testlink_id):
         '''
         Register this testcase name with one or more testlink id\n
         Note: If 1-1 link setting method is selected. Only the first id param will be used\n
         '''
-        if testlink_id:
-            iTC = BuiltIn().get_variable_value('${TESTLINK_iTC}')
-            iTC.testlink_id = testlink_id[0].strip()
-        
+        try:            
+            if testlink_id:
+                iTC = BuiltIn().get_variable_value('${TESTLINK_iTC}')
+                iTC.testlink_id = testlink_id[0].strip()
+        except Exception, err:
+            pass
